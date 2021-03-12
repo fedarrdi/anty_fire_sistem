2c3470084bc7adf8238eea2622186b4d7a75e7fd
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-03-12 14:26:06 +0200

    Add files via upload

--- ske.ino
+++ ske.ino
@@ -1,13 +1,13 @@
-#include<malloc.h>
-
-#define T_PIN 1
+#define T_PIN 5
+#define H_PIN 5
 
 typedef struct
 {
-  byte size; // number of bytes
+  byte size_; // number of bytes
   byte pin; //pin where we will read the data from
-  byte data[]; //value_of_data
-  char type[]; //type of data as a string
+  byte offset;
+  byte *data;
+  char *type; //type of data as a string
 }Module;
 
 byte read_data(byte pin)
@@ -26,38 +26,33 @@ byte read_data(byte pin)
   return result;
 }
 
-void fill_data(Module module, byte offset)
+void fill_data(Module *module)
 {
   
-  digitalWrite(DHpin, LOW); //Pull down the bus to send the start signal
+  digitalWrite(module->pin, LOW); //Pull down the bus to send the start signal
   delay(30); //The delay is greater than 18 ms so that DHT 11 can detect the start signal
-  digitalWrite(DHpin, HIGH);
+  digitalWrite(module->pin, HIGH);
   delayMicroseconds(40); //Wait for DHT11 to respond
   
-  pinMode(DHpin, INPUT);
-  while(digitalRead(DHpin) == HIGH);
+  pinMode(module->pin, INPUT);
+  while(digitalRead(module->pin) == HIGH);
   delayMicroseconds(80); //The DHT11 responds by pulling the bus low for 80us;
   
-  if(digitalRead(DHpin) == LOW)
+  if(digitalRead(module->pin) == LOW)
     delayMicroseconds(80); //DHT11 pulled up after the bus 80us to start sending data;
   
-  for(int i = offset; i < module.size; module.data[i++] = read_data(module.pin)) //Receiving temperature and humidity data, check bits are not considered;
+  for(int i = module->offset; i < module->size_; module->data[i++] = read_data(module->pin)) //Receiving temperature and humidity data, check bits are not considered;
   
-  pinMode(DHpin, OUTPUT);
-  digitalWrite(DHpin, HIGH); //After the completion of a release of data bus, waiting for the host to start the next signal
+  pinMode(module->pin, OUTPUT);
+  digitalWrite(module->pin, HIGH); //After the completion of a release of data bus, waiting for the host to start the next signal
 }
 
-void print_data(Module module)
+void print_data(Module *module)
 {
-  Serial.print(module.type);  
-  for(byte i = 0;i < module.size;i++)
-    Serial.println(module.data[i]);
-}
-
-byte *get_memory(byte n)
-{
-  byte *res = malloc(sizeof *res * n);
-  return res;
+  Serial.print(module->type);  
+  for(byte i = 0;i < module->size_;i++)
+    Serial.println(module->data[i]);
+  Serial.print(" ");
 }
 
 void str_cpy(char *l, const char *r)
@@ -66,38 +61,36 @@ void str_cpy(char *l, const char *r)
     *l = *r, l++, r++;
 }
 
-byte *get_type(char *str, byte n)
+void create_module(Module *module, byte size_, byte PIN, byte offset, char *str, byte n)
 {
-  char *type = malloc(sizeof a * n);
-  str_cpy(type, str);
-  return type;
+  module->size_ = size_;
+  module->pin = PIN;
+  module->offset = offset;
+  module->type = (char*)malloc(sizeof module->type * n + 1);  
+  module->data = (byte*)malloc(sizeof(byte) * module->size_);
+  str_cpy(module->type, str);
 }
 
-//-------------------------------------------------------------------------------------------------------------------------
 
+Module *temperature;
+Module *humidity;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(DHpin, OUTPUT);
-  Module temperature = {2, T_PIN, get_memory(2), get_type("Temperature", sizeof("Temperature"))};
-  Module humidity = {};
-}
+  
+  pinMode(T_PIN, OUTPUT);
+  pinMode(H_PIN, OUTPUT);
 
+  create_module(temperature, 2, T_PIN, 2, "Temperature", sizeof("Temperature"));
+  create_module(humidity, 2, H_PIN, 0, "Humidity", sizeof("Humidity"));
+}
 
 void loop()
 {
-  /*get_data();
-  
-  Serial.print("Humdity = ");
-  Serial.print(dat[0], DEC); //Displays the integer bits of humidity;
-  Serial.print('.');
-  Serial.print(dat[1], DEC); //Displays the decimal places of the humidity;
-  Serial.println('%');
-  Serial.print("Temperature = ");
-  Serial.print(dat[2], DEC); //Displays the integer bits of temperature;
-  Serial.print('.');
-  Serial.print(dat[3], DEC); //Displays the decimal places of the temperature;
-  Serial.println('C');*/
-  
+  fill_data(temperature);
+  print_data(temperature);
+
+  fill_data(humidity);
+  print_data(temperature);
 }

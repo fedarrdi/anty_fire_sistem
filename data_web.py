e9b1142604c87e69e832332c4a988604b1fd615d
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-03-14 01:47:14 +0200

    final push bugs removed from py file
    Very hard one.

--- data_web.py
+++ data_web.py
@@ -2,7 +2,6 @@ import streamlit as st
 import matplotlib.pyplot as pls
 import pandas as pd
 import re
-import sys
 import time
 from urllib.request import urlopen
 
@@ -10,19 +9,14 @@ url0 ='http://192.168.1.184/flame'
 url1 = 'http://192.168.1.184/humidity_temperature'
 
 df1 = pd.DataFrame({'Temp: ':[0.0],'Hum: ':[0.0]})
-df2 = pd.DataFrame({'flame': [False]})
-@st.cache
+df2 = pd.DataFrame({'flame': [0]})
+
 def update_flame():
     global df2
-    try:
-        page1 = urlopen(url0)
-        html1 = page.read().decode('utf-8')
-        status_text2.write(html1)
-        st.write(html1)
-        splittext1 = re.split("[ ]", html1)
-        is_flame = bool(splittext1[1])
-    except:
-        is_flame = False
+    page1 = urlopen(url0)
+    html1 = page1.read().decode('utf-8')       
+    splittext1 = re.split("[ ]", html1)
+    is_flame = int(splittext1[1])
     df2 = df2.append({'flame': is_flame}, ignore_index = True)
 
 def update_temp_hum():
@@ -30,7 +24,6 @@ def update_temp_hum():
     try:
         page = urlopen(url1)
         html = page.read().decode('utf-8')
-        status_text1.write(html)
         splittext = re.split('[ %C]', html)
         hum = float(splittext[2])
         temp = float(splittext[5])
@@ -52,4 +45,3 @@ for i in range(0, 500):
     chart_weather2.add_rows(df2[:][len(df2)-1: len(df2)])
     status_text2.write(df2.loc[:][::-1])
     time.sleep(1)
-

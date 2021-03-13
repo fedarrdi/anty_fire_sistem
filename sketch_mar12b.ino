c12f8dafb5c9ca92f4894c8b27452c4114adfa73
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-03-13 13:01:41 +0200

    Adding Web Server and fixing code
    Web Server is Added , now you can view data in real time  in the internet.
    The code is written for sixth time.
    Problems solved for now:
    1.Pointer problems.
    2.Problems with hardware.
    3.Buggy Module structure.
    4.Removing Module pointer functions.
    5.Inaccessibility issues.

--- sketch_mar12b.ino
+++ sketch_mar12b.ino
@@ -1,98 +1,150 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#define STASSID "Ndd_wrt"
+#define STAPSK  "tania123"
+
+ESP8266WebServer server(80);
+
 struct Module
 {
   byte size_;
   byte pin;
   byte *data;
-  void (*fill_data)(struct Module *module);
-  void (*print_data)(const struct Module *module);
-};
+} humidity_temperature_module, flame_module;
 
-void print_humidity_temperature(const struct Module *module)
+void fill_flame_data_module()
 {
-  Serial.print("Humdity = ");
-  Serial.print(module->data[0], DEC); 
-  Serial.print('.');
-  Serial.print(module->data[1], DEC); 
-  Serial.println('%');
-  Serial.print("Temperature = ");
-  Serial.print(module->data[2], DEC);
-  Serial.print('.');
-  Serial.print(module->data[3], DEC);
-  Serial.println('C');
-  byte checksum = module->data[0] + module->data[1] + module->data[2] + module->data[3];
-  if (module->data[4] != checksum) 
-    Serial.println("-- Checksum Error!");
-  else
-    Serial.println("-- OK");
+  flame_module.data[0] = analogRead(flame_module.pin);
 }
-void fill_flame_data_module(struct Module *module)
+
+void print_flame_data_module()
 {
-  module->data[0] = analogRead(module->pin); 
-}
-void print_flame_data_module(const struct Module *module)
-{
-  if(module->data[0] >= 33) // if flame isn't detected
-    Serial.println("NO"); // turn OFF Arduino's LED
-  else
-    Serial.println("YES"); // turn ON Arduino's LED
+  String msg = "Yes";
+  if (flame_module.data[0] >= 33)
+    msg = "No";
+  server.send(200, "text/html", msg);
 }
 
 byte read_data_humidity_temperature(byte pin)
 {
-  byte i = 0;
   byte result = 0;
-  for (i = 0; i < 8; i++) {
-      while (digitalRead(pin) == LOW);
+  for (byte i = 0; i < 8; i++)
+  {
+    while (digitalRead(pin) == LOW);
       delayMicroseconds(30); 
       if (digitalRead(pin) == HIGH)
         result |= (1 << (8 - i)); 
     while (digitalRead(pin) == HIGH);
-    }
+  }
   return result;
 }
 
-void fill_data_humidity_temperature(struct Module *module)
+void fill_data_humidity_temperature()
 {
-  digitalWrite(module->pin, LOW);
-  delay(30); 
-  digitalWrite(module->pin, HIGH);
-  delayMicroseconds(40); 
-  pinMode(module->pin, INPUT);
-  while(digitalRead(module->pin) == HIGH);
-  delayMicroseconds(80); 
-  if(digitalRead(module->pin) == LOW)
-    delayMicroseconds(80); 
-  for(int i = 0; i < module->size_; i++) 
-    module->data[i] = read_data_humidity_temperature(module->pin);
-  pinMode(module->pin, OUTPUT);
-  digitalWrite(module->pin, HIGH);
+  digitalWrite(humidity_temperature_module.pin, LOW);
+  delay(30);
+  digitalWrite(humidity_temperature_module.pin, HIGH);
+  delayMicroseconds(40);
+  pinMode(humidity_temperature_module.pin, INPUT);
+  while (digitalRead(humidity_temperature_module.pin) == HIGH);
+  delayMicroseconds(80);
+  if (digitalRead(humidity_temperature_module.pin) == LOW)
+    delayMicroseconds(80);
+  for (int i = 0; i < humidity_temperature_module.size_; i++)
+    humidity_temperature_module.data[i] = read_data_humidity_temperature(humidity_temperature_module.pin);
+  pinMode(humidity_temperature_module.pin, OUTPUT);
+  digitalWrite(humidity_temperature_module.pin, HIGH);
 }
 
-void create_module(struct Module *module, byte size_, byte pin, void (*fill_)(struct Module *module), void (*print_)(const struct Module *module))
+void print_humidity_temperature()
+{
+  String msg = "<br>";
+  msg += "Humdity = ";
+  msg += String(humidity_temperature_module.data[0]);
+  msg += '.';
+  msg += String(humidity_temperature_module.data[1]);
+  msg += "%</br><br>";
+  msg += "Temperature = ";
+  msg += String(humidity_temperature_module.data[2]);
+  msg += '.';
+  msg += String(humidity_temperature_module.data[3]);
+  msg += "C</br>";
+  server.send(200, "text/html", msg);
+}
+
+void create_module(struct Module *module, byte size_, byte pin)
 {
   module->size_ = size_;
   module->pin = pin;
   module->data = (byte*)malloc(sizeof(byte) * module->size_);
-  module->fill_data = fill_;
-  module->print_data = print_;
   pinMode(module->pin, OUTPUT);
 }
 
-struct Module humidity_temperature_module, flame_module;
+//----functions to handle pages --------------------------------------
+
+void handleRoot()
+{
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
+
+void handleNotFound()
+{
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++)
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+
+  server.send(404, "text/html", message);
+}
+//--------------------------------------------------------------------
+
+void setup_server()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+
+  while (WiFi.status() != WL_CONNECTED)
+    delay(500);
+    Serial.print(".");
+
+  Serial.print(WiFi.localIP());
+
+  
+  if (MDNS.begin("esp8266"))
+    Serial.println("MDNS responder started");
+
+  server.on("/", handleRoot);
+  server.on("/humidity_temperature", HTTP_GET, print_humidity_temperature);
+  server.on("/flame_data_module", HTTP_GET, print_flame_data_module);
+
+  server.onNotFound(handleNotFound);
+  server.begin();
+}
 
 void setup()
 {
   Serial.begin(9600);
-  create_module(&humidity_temperature_module, 4, 5, fill_data_humidity_temperature, print_humidity_temperature);
-  
-  create_module(&flame_module, 1, A0, fill_flame_data_module, print_flame_data_module);
+  setup_server();
+  create_module(&humidity_temperature_module, 4, 5);
+  create_module(&flame_module, 1, A0);
 }
 
 void loop()
 {
-  //humidity_temperature_module.fill_data(&humidity_temperature_module);
-  //humidity_temperature_module.print_data(&humidity_temperature_module);
-  flame_module.fill_data(&flame_module);
-  flame_module.print_data(&flame_module);
-  delay(1000);
+  server.handleClient();
+  fill_data_humidity_temperature();
+  fill_flame_data_module();
+  delay(3600);
 }
